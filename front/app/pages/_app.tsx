1198addda8b3979e51014b0c872d05ded02ce3ee
Yusuke-Ijima2 <yusuke9251120@gmail.com>
2022-07-14 14:33:11 +0900

    Assets, Metadata, and CSS #3

--- front/app/pages/_app.tsx
+++ front/app/pages/_app.tsx
@@ -1,9 +1,5 @@
-import React from 'react'
-import {AppProps} from 'next/app';
-import '../styles/globals.css';
+import "../styles/global.css";
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
-
-export default MyApp
