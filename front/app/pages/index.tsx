3222baf1f7c0b461c4811f73913ab987b7b9980d
Yusuke-Ijima2 <yusuke9251120@gmail.com>
2022-07-15 11:13:30 +0900

    Dynamic Routes , API Routes #3

--- front/app/pages/index.tsx
+++ front/app/pages/index.tsx
@@ -1,6 +1,8 @@
 import Layout from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
 
 // export async function getStaticProps() {
 //   const allPostsData = getSortedPostsData();
@@ -22,19 +24,18 @@ export async function getServerSideProps() {
 export default function Home({ allPostsData }) {
   return (
     <Layout home>
-      {/* Keep the existing code here */}
-
-      {/* Add this <section> tag below the existing <section> tag */}
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
